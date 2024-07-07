f3ae6a996a37bf7325a875560247f07eb42dfe04
liansun96 <toetetaung3296@gmail.com>
2024-07-07 11:18:13 +0630

    connect to database

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+//connectDB
+const connectDB  = require('./db/connect')
+
+//routes
+app.use('/api/v1/auth' , authRouter)
+app.use('/api/v1/jobs' , jobsRouter)
+
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+
 
 //routes
 app.get("/", (req, res) => {
@@ -21,6 +32,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening on prot ${port}...`));
   } catch (error) {
     console.log(error);
