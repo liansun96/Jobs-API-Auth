e094ec018e891ffb0b2037122b32eab3a57c1b29
liansun96 <toetetaung3296@gmail.com>
2024-07-10 23:41:59 +0630

    updated user model

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
     ],
-    uniqure: true,
+    unique: true,
   },
   password: {
     type: String,
