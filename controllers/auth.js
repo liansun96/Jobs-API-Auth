24b7e4d4a2713006288959507bf065d44151c56c
liansun96 <toetetaung3296@gmail.com>
2024-07-12 23:21:23 +0630

    update user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-    console.log(name , email , password);
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password , salt)
-    console.log(hashedPassword);
-    const tempUser = {name , email , password : hashedPassword}
-    console.log(tempUser);
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
