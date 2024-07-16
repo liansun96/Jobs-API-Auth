6f46dc95a971a827c84e4ecc546c920e3744319d
liansun96 <toetetaung3296@gmail.com>
2024-07-16 11:07:00 +0630

    apply login logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,32 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
+const BadRequestError = require("../errors/bad-request");
+const UnauthenticatedError = require("../errors/unauthenticated");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { user: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please Provide Email & Password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invilid Crendentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("invilid Crendentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
