1da49524b288d45bc5ca60a4e5016eaebd0383e2
liansun96 <toetetaung3296@gmail.com>
2024-07-21 23:24:14 +0630

    update jobs controler

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,18 +2,30 @@ const { StatusCodes } = require("http-status-codes");
 const Job = require("../models/Job");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy : req.user.userId}).sort('createdAt')
-  res.status(StatusCodes.OK).json({ count: jobs.length , jobs})
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ count: jobs.length, jobs });
 };
 
 const getJob = async (req, res) => {
-  res.send("Get Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json({job})
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
