5f69bb6bd9d8fd49bf4a8563f13b729a5bb4f4ae
liansun96 <toetetaung3296@gmail.com>
2024-07-30 23:19:14 +0630

    adding extra security packages & apply them

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const app = express();
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors()) 
+app.use(xss())
+
 //connectDB
 const connectDB  = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
